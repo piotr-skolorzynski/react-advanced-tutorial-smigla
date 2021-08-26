873c7c03214879b4c20892d45e0aa115b2d353cd
Piotr <p.skolorzynski@gmail.com>
2021-08-26 11:20:39 +0200

    create UseStateBasics.js file create UseStateBasics component as example why we need to use useState hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { ErrorExample } from './useState/ErrorExample';
+// import { ErrorExample } from './useState/ErrorExample';
+
+import UseStateBasics from "./useState/UseStateBasics";
 
 function App() {
   return (
     <div className="container">
-      <ErrorExample />
+      {/* <ErrorExample /> */}
+      <UseStateBasics />
     </div>
   )
 }
---
+++ src/useState/UseStateBasics.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+const UseStateBasics = () => {
+    // console.log(useState('hello world'));
+    // const value = useState(1)[0];
+    // const handler = useState(1)[1];
+    // console.log(value, handler);
+
+    const [text, setText] = useState('random title'); //tutaj podaje się default value, więc jeśli takiej nie ma to daj null
+
+    const handleClick = () => {
+        if (text === 'random title') {
+            setText('hello world !!!'); //dzięki useState zmieniamy wartość zmiennej text i można ją wyrenderować
+        } else {
+            setText('random title');
+        }
+    }
+
+    return (
+        <React.Fragment>
+            <h1>{text}</h1>
+            <button className="btn" onClick={handleClick}>change title</button>
+        </React.Fragment>
+    )
+};
+
+export default UseStateBasics;
