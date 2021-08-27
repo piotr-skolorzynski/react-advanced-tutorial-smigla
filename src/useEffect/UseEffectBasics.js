ebf7db5deb172d058f6b4c9abb523b18b567b200
Piotr <p.skolorzynski@gmail.com>
2021-08-27 15:15:41 +0200

    modify UseEffectBasics.js add example of using dependency list

--- src/useEffect/UseEffectBasics.js
+++ src/useEffect/UseEffectBasics.js
@@ -15,12 +15,27 @@ const UseEffectBasics = () => {
         setValue(value + 1)
     }
 
-    //funkcja useEffect odpala callback za każdym razem gdy dochodzi do ponownego renderowania strony
+    //funkcja useEffect odpala callback za każdym razem gdy dochodzi do ponownego renderowania strony, przykład bez drugiego argumnetu
+    // useEffect(() => {
+    //     console.log('call useEffect');
+    //     //example of using conditional in callback function to re-render
+    //     if (value > 0) document.title = `New Messages(${value})`
+    // });
+
+    //przykład useEffect z wykorzystaniem drugiego argumentu tzw. dependency list w postaci tabblicy,
+    // pozwala określić jakiej zależności ma dotyczyć renderowanie
+    // jeżeli jest pusta to odpala renderowanie tylko raz przy tzw. initial render
+    // wewnątrz tablicy można wypisać tyle zależności ile chcemy żeby wykonał się callback w tym useEffect
     useEffect(() => {
         console.log('call useEffect');
         //example of using conditional in callback function to re-render
         if (value > 0) document.title = `New Messages(${value})`
-    })
+    }, [value]);
+
+    useEffect(() => {
+        console.log('hello world');
+    }, []);
+
     console.log('render component');
     return (
         <>
