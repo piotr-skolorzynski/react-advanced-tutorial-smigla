3bf717e32545294d8b87e5c695686ae765b77bd4
Piotr <p.skolorzynski@gmail.com>
2021-08-31 16:18:44 +0200

    modify ControlledInputs.js render submited person on page

--- src/forms/ContolledInputs.js
+++ src/forms/ContolledInputs.js
@@ -10,10 +10,20 @@ const ContolledInputs = () => {
 
     const [firstName, setFirstName] = useState('');
     const [email, setEmail] = useState('');
+    const [people, setPeople] = useState([]);
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(firstName, email);
+        if (firstName && email) {
+            const person = { id: new Date().getTime().toString(), firstName, email };
+            setPeople(people => {
+                return [...people, person];
+            });
+            setFirstName('');
+            setEmail('');
+        } else {
+            console.log('empty values');
+        }
     }
 
     return (
@@ -30,6 +40,15 @@ const ContolledInputs = () => {
                     </div>
                     <button type="submit">add person</button>
                 </form>
+                {
+                    people.map(person => {
+                        const {id, firstName, email} = person;
+                        return <div className="item" key={id}>
+                            <h4>{firstName}</h4>
+                            <p>{email}</p>
+                        </div>
+                    })
+                }
             </article>
         </>
     );
