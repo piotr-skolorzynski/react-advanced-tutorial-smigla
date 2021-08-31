2127ecdd0c1ea84c8cace3ab4df6efc2b75e5d25
Piotr <p.skolorzynski@gmail.com>
2021-08-31 15:49:32 +0200

    modify ControlledInputs.js chain each input values with its state using onChange event

--- src/forms/ContolledInputs.js
+++ src/forms/ContolledInputs.js
@@ -8,9 +8,12 @@ import { useState } from "react";
 
 const ContolledInputs = () => {
 
+    const [firstName, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+
     const handleSubmit = e => {
         e.preventDefault();
-        console.log('hello world!!!');
+        console.log(firstName, email);
     }
 
     return (
@@ -19,11 +22,11 @@ const ContolledInputs = () => {
                 <form className="form" onSubmit={handleSubmit}>
                     <div className="form-control">
                         <label htmlFor="firstName">Name : </label>
-                        <input type="text" id="firstName" name="firstName"/>
+                        <input type="text" id="firstName" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)}/>
                     </div>
                     <div className="form-control">
                         <label htmlFor="email">Email : </label>
-                        <input type="text" id="email" name="email"/>
+                        <input type="text" id="email" name="email" value={email} onChange={e => setEmail(e.target.value)}/>
                     </div>
                     <button type="submit">add person</button>
                 </form>
