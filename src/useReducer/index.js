3d3f91f365ca2a28b31dfe18c9055947ca8aca53
Piotr <p.skolorzynski@gmail.com>
2021-09-10 12:06:03 +0200

    modify index.js file add example how to use Reducer on example adding item to the list

--- src/useReducer/index.js
+++ src/useReducer/index.js
@@ -3,16 +3,34 @@ import Modal from './Modal';
 import { data } from '../data';
 
 //reducer function
-// w useReducer pierwszym argumentem jest funkcja zwana reducer i jak chcesz zrobić coś ze stanem to za każdym razem używasz dispatch. Reducer bierze stary state i działa z nim i zwraca nowy stan. Reducer wyrzuca się poza komponent. Drugi komponent to tzw. initial state (dane wejściowe), też można to wyrzucić poza komponent
+// w useReducer pierwszym argumentem jest funkcja zwana reducer i jak chcesz zrobić coś ze stanem to za każdym razem używasz dispatch. Reducer bierze stary state i działa z nim i zwraca!  nowy stan (bez zwracania nowego stanu działanie tej funckji nie ma sensu i wywala błąd). Reducer wyrzuca się poza komponent. Drugi komponent to tzw. initial state (dane wejściowe), też można to wyrzucić poza komponent
 
 const reducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
+        const newPeople = [...state.people, action.payload]
+        return {
+            ...state,
+            people: newPeople,
+            isModalOpen: true,
+            modalContent: 'item added'
+            }
+    }
 
+    if(action.type === 'NO_VALUE') {
+        return {
+            ...state,
+            isModalOpen: true,
+            modalContent: 'please enter value'
+        }
+    }
+
+    throw new Error('no matching action type');
 };
 
 const defaultState = {
     people: [],
     isModalOpen: false,
-    modalContent: 'hello world!'
+    modalContent: ''
 }
 
 const Index = () => {
@@ -22,9 +40,15 @@ const Index = () => {
     const handleSubmit = e => {
         e.preventDefault();
         if(name) {
-
+            const newItem = {
+                id: new Date().getTime().toString(),
+                name
+            }
+            //w dispatch zawsze podajemy obiekt
+            dispatch({type:'ADD_ITEM', payload: newItem});
+            setName('');
         } else {
-
+            dispatch({type: 'NO_VALUE'}); //nadanie nazwy typowi akcji który służy do generowanie błędów
         }
     }
 
